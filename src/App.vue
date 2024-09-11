f7d88c41c39a96e6b2cdba597da79c51cf621729
JohanSabino <jasc9494@gmail.com>
2024-09-11 14:24:33 -0500

    Se agrega el proyecto

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="unisimon logo" src="./assets/uso logo.png">
+  <headerPage msg="INVENTARIO DE PRUEBAS DEL PROGRAMA DE PSICOLOGIA"/>
+</template>
+
+<script>
+import HeaderPage from './components/HeaderPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
